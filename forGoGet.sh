5e98f7302c9fc55b45a7643abcd77a4ae3ee6b7a
sunliang <sunliang711@163.com>
2017-03-18 22:52:31 +0800

    go 代码依赖处理脚本

---
+++ forGoGet.sh
@@ -0,0 +1,15 @@
+#第一步,找出所有导入的包
+#下面两个正则分别对应块导入和单行导入
+find .  -name "*.go" -print0|xargs -0 sed -rn -e "/^import\s+\(/,/\)/p" -e "/^import\s+[^(]+/p" > allImports
+
+#第二步,过滤掉自带的包(依据是有无反斜杠)和注释语句
+#通过观察发现github.com和golang.org上面在包的路径至少有两个反斜杠
+#所以抽取形如 "xxxx/xxx/xxx"的import路径即可 另外第一部分要有点号
+# grep -Po '(?<=")[^/]+/[^/]+/[^/]+[^"]+(?=")' allImports | sort |uniq >result
+grep -Po '(?<=")[^.]+\.[^.]+/[^/]+/[^/]+[^"]+(?=")' allImports | sort |uniq >result
+
+#注意点,go get使用的是版本管理工具下载的,如果有需要的话,先设置git的proxy
+while read import;do
+    echo "go get $import ..."
+    go get $import
+done<result
